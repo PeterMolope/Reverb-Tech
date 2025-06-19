cb0a4f5e426c674e8248157a5a2bd295ec18e007
PeterMolope <ptrmolope@gmail.com>
2025-06-19 15:13:46 +0200

    setting up backend server

--- my-app/backend/server.js
+++ my-app/backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 
 const app = express();
+const _PORT_ = 8000;
 
-app.listen(8000,()=>{
+app.listen(_PORT_,()=>{
+
+    console.log("Server is now running on port no. " +_PORT_+"!");
 
 });
