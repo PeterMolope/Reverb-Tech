f01cf9a305778c684e446b1311c5e08b297a007a
PeterMolope <ptrmolope@gmail.com>
2025-06-19 15:56:15 +0200

    setting up the auth route

--- my-app/backend/server.js
+++ my-app/backend/server.js
@@ -1,8 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 
+//routes
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config();
 const app = express();
-const _PORT_ = 8000;
+const _PORT_ = process.env.PORT;
 
+app.use("/api/auth", authRoutes);
 app.listen(_PORT_,()=>{
 
     console.log("Server is now running on port no. " +_PORT_+"!");
