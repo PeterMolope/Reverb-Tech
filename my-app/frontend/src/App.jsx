3842c9e793a1a759965c3ff310ea2fd529f9b530
PeterMolope <ptrmolope@gmail.com>
2025-06-24 12:35:32 +0200

    Navbar component

--- my-app/frontend/src/App.jsx
+++ my-app/frontend/src/App.jsx
@@ -6,13 +6,22 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
+    <div className="min-h-screen bg-zinc-900 font-sans antialiased text-gray-100">
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(0,0,0,0.8)_0%,rgba(20,20,100,0.6)_45%,rgba(0,0,0,0.9)_100%)]" />
+        </div>
+      </div>
+
+      <div className="relative z-50 pt-20">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
