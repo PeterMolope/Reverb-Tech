09307c1d7f4856980981bd9e8b7225a632c940d6
PeterMolope <ptrmolope@gmail.com>
2025-06-24 23:09:56 +0200

    handle orders

--- my-app/frontend/src/App.jsx
+++ my-app/frontend/src/App.jsx
@@ -9,12 +9,20 @@ import LoadingSpinner from "./components/LoadingSpinner";
 import { useEffect } from "react";
 import AdminPage from "./pages/AdminPage";
 import CategoryPage from "./pages/CategoryPage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   const { user, checkAuth, checkingAuth } = useUserStore();
+  const { getCartItems } = useCartStore();
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
+
+  useEffect(() => {
+    if (!user) return;
+
+    getCartItems();
+  }, [getCartItems, user]);
 
   if (checkingAuth) return <LoadingSpinner />;
   return (
@@ -54,6 +62,10 @@ function App() {
             }
           />
           <Route path="/category/:category" element={<CategoryPage />} />
+          <Route
+            path="/cart"
+            element={user ? <CartPage /> : <Navigate to="/login" />}
+          />
         </Routes>
       </div>
       <Toaster
