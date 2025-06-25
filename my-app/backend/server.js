f41df71b2327e9178bc3c93f95fc09981d0a218e
PeterMolope <ptrmolope@gmail.com>
2025-06-25 11:02:59 +0200

    deployment setup

--- my-app/backend/server.js
+++ my-app/backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 //routes
 import authRoutes from "./routes/auth.route.js";
@@ -16,6 +17,8 @@ dotenv.config();
 const app = express();
 const _PORT_ = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
@@ -26,6 +29,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(_PORT_, () => {
   console.log("Server is now running on port no. " + _PORT_ + "!");
